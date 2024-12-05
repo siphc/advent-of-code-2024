9b3bee429d65345d9e502af0af7b7f36fb887519
siphc <william.yf.pan@outlook.com>
2024-12-05 00:58:04 -0800

    Day 5

---
+++ 05-B.cpp
@@ -0,0 +1,42 @@
+// the input schema requires a tiny tweak to the input file.
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+    std::ifstream input("05.txt");
+
+    std::vector<std::vector<int>> order(100);
+    int res = 0;
+
+    std::string s;
+    input >> s;
+    while (s.length()==5){
+        order.at(stoi(s.substr(0,2))).push_back(stoi(s.substr(3,2)));
+        input >> s;
+    }
+
+    // this is terrible time complexity. too bad!
+    while (input.peek()!=-1){
+        input >> s;
+        std::vector<int> currupdate;
+        bool sanity = false;
+
+        for (int i=0; i<(s.length()+1)/3; i++){
+            currupdate.push_back(stoi(s.substr(3*i,2)));
+            for (int j=0; j<currupdate.size()-1; j++){
+                std::vector<int>* currpage = &order.at(currupdate.back());
+                if (std::find((*currpage).begin(), (*currpage).end(), currupdate.at(j)) != (*currpage).end()){
+                    std::swap(currupdate.back(), currupdate.at(j));
+                    sanity = true;
+                }
+            }
+        }
+
+        // std::cout << s << ' ' << sanity << std::endl;
+        if (sanity) res+=currupdate.at(currupdate.size()/2);
+    }
+
+    std::cout << res << std::endl;
+}
